cd87c0f285f8796c74be06cadfe634781aa46466
kieter <balisnom@ualberta.ca>
2023-10-26 23:19:09 -0700

    feat: login error messages

--- routes/authRoute.ts
+++ routes/authRoute.ts
@@ -1,21 +1,46 @@
 import express from "express";
 import passport from 'passport';
 import { forwardAuthenticated } from "../middleware/checkAuth";
-
+import {SessionData} from "express-session";
 const router = express.Router();
 
+declare module 'express-session' {
+  export interface SessionData {
+    message?: string;
+  }
+}
+
 router.get("/login", forwardAuthenticated, (req, res) => {
-  res.render("login");
+  const message = req.session.message || '';
+  res.render("login", { message });
 })
 
-router.post(
-  "/login",
-  passport.authenticate("local", {
-    successRedirect: "/dashboard",
-    failureRedirect: "/auth/login",
-    /* FIX ME: 😭 failureMsg needed when login fails */
-  })
-);
+router.post("/login", (req, res, next) => {
+  passport.authenticate("local", (err, user, info) => {
+    if (err) {
+      console.log('err', err);
+      return next(err);
+    }
+    if (!user) {
+      console.log('user', user);
+      console.log('info', info)
+      if (info.message === 'Missing credentials') {
+        req.session.message = `Couldn't find user with email: ${req.body.email}`;
+      }
+      if (info.message === 'Your login details are not valid. Please try again') {
+        req.session.message = `Password is incorrect`
+      }
+      return res.redirect("/auth/login");
+    }
+    req.logIn(user, (err) => {
+      if (err) {
+        return next(err);
+      }
+      return res.redirect("/dashboard");
+    });
+  })(req, res, next);
+});
+
 
 router.get("/logout", (req, res) => {
   req.logout((err) => {
